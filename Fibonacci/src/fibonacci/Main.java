79ff9305adcce31d0c08edaa0ce14a8fcad2227a
matychp <matychp@gmail.com>
2014-10-01 15:44:08 -0300

    Quitado el metodo mejorado, no funcionaba para ciertos casos.

--- Fibonacci/src/fibonacci/Main.java
+++ Fibonacci/src/fibonacci/Main.java
@@ -16,7 +16,6 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println(addFibonacciEvenValues(100));
-        System.out.println(addFibonacciEvenValuesBetter(100));
 
     }
 
